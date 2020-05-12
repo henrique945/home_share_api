e778419b2bc3aff99bb3051690d140df3378b300
henrique945 <henrique.rodrigues7@hotmail.com>
2020-05-12 20:25:03 -0300

    update(ssl): turn on ssl

--- src/modules/typeorm/services/type-orm.service.ts
+++ src/modules/typeorm/services/type-orm.service.ts
@@ -77,7 +77,7 @@ export class TypeOrmService implements TypeOrmOptionsFactory {
         password: this.env.DB_PASSWORD,
         acquireTimeout: this.env.DB_TIMEOUT,
         extra: {
-          ssl: false,
+          ssl: true,
         },
       });
     } else if (this.env.DB_TYPE === 'sqlite') {
